5270c1c587c20bf821b7920b77f0f3dcb554914d
Jamal Boulhous <jamal.b@onthegosystems.com>
2021-12-28 13:29:25 +0100

    formatting

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/vendor/advanced-custom-
 include_once( MY_ACF_PATH . 'acf.php' );
 
 // Customize the url setting to fix incorrect asset URLs.
-add_filter('acf/settings/url', 'my_acf_settings_url');
+add_filter( 'acf/settings/url', 'my_acf_settings_url' );
 function my_acf_settings_url( $url ) {
 	return MY_ACF_URL;
 }
